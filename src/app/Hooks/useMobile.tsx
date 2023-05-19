ec0681c140c2ec2a92535fe0f7ec2bde8dc272bf
ivan01-tech <ivansilatsa@gmail.com>
2023-05-19 20:39:08 +0200

    adding the header

--- src/app/Hooks/useMobile.tsx
+++ src/app/Hooks/useMobile.tsx
@@ -3,6 +3,7 @@
 import { MobileMenuContext } from "@/app/Context/MobileMenu";
 import { useContext } from "react";
 
+
 export function useMobile() {
   return useContext(MobileMenuContext);
-}
+}    
