956e7f232576d56d5268a6ae042e069c60aa242c
ivan01-tech <ivansilatsa@gmail.com>
2023-05-20 11:33:26 +0200

    adding logos and the skillsObj

---
+++ src/app/utils/skillsObj.ts
@@ -0,0 +1,70 @@
+export const skillsObj: { name: string; path: string }[] = [
+  {
+    name: "HTML",
+    path: "/public/logos/HTML5_logo_and_wordmark.svg.png",
+  },
+  {
+    name: "CSS",
+    path: "/public/logos/css3.png",
+  },
+  {
+    name: "JavaScript",
+    path: "/public/logos/js.png",
+  },
+  {
+    name: "TypeScript",
+    path: "/public/logos/js.png",
+  },
+  {
+    name: "ReactJS",
+    path: "/public/logos/React-icon.svg.png",
+  },
+  {
+    name: "NextJS",
+    path: "/public/logos/Nextjs-logo.svg",
+  },
+  {
+    name: "NodeJS",
+    path: "/public/logos/590px-Node.js_logo.svg.png",
+  },
+  {
+    name: "NestJS",
+    path: "/public/logos/nestjs-small.svg",
+  },
+  {
+    name: "Express",
+    path: "/public/logos/expressjs-icon.svg",
+  },
+  {
+    name: "MongoDB",
+    path: "/public/logos/mongodb.svg",
+  },
+  {
+    name: "Sql",
+    path: "",
+  },
+  {
+    name: "Firebase",
+    path: "",
+  },
+  {
+    name: "Neo4j",
+    path: "/public/logos/neo4j-logo-2020-1.svg",
+  },
+  {
+    name: "Redux",
+    path: "/public/logos/redux.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    path: "/public/logos/tailwindcss-icon.svg",
+  },
+  {
+    name: "Bootstrap",
+    path: "/public/logos/bootstarp.png",
+  },
+  {
+    name: "Material UI",
+    path: "/public/logos/mui.png",
+  },
+];
